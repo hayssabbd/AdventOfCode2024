578ff4c185d5672ea4ef254ca96e5d124f40a2ed
Craig Deacon <craig.deacon@bbd.ca>
2024-12-01 11:10:42 -0500

    Removing comments and pointing at correct file.

--- Main.java
+++ Main.java
@@ -17,26 +17,13 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
 
-    // File path is passed as parameter
-    File file = new File("C:\\Documents\\file_name.txt");
+    File file = new File("C:\\Docs\\advent.txt");
 
-    // Note:  Double backquote is to avoid compiler
-    // interpret words
-    // like \test as \t (ie. as a escape sequence)
+    BufferedReader br = new BufferedReader(new FileReader(file));
 
-    // Creating an object of BufferedReader class
-    BufferedReader br
-        = new BufferedReader(new FileReader(file));
-
-    // Declaring a string variable
-    String st;
-    // Condition holds true till
-    // there is character in a string
-    while ((st = br.readLine()) != null)
-
-    // Print the string
-    {
-      System.out.println(st);
+    String line;
+    while ((line = br.readLine()) != null) {
+      System.out.println(line);
     }
   }
 }

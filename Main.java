b08cf96d05e15b4240c2f9e24968f79c051dae74
Craig Deacon <craig.deacon@bbd.ca>
2024-12-01 11:20:51 -0500

    Day 1 puzzle 2.

--- Main.java
+++ Main.java
@@ -12,18 +12,45 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
 
     File file = new File("C:\\Docs\\advent.txt");
-
     BufferedReader br = new BufferedReader(new FileReader(file));
 
     String line;
+    List<Integer> firstColumn = new ArrayList<>();
+    List<Integer> secondColumn = new ArrayList<>();
+
     while ((line = br.readLine()) != null) {
-      System.out.println(line);
+      String firstNumber = line.substring(0,5);
+      String secondNumber = line.substring(8,13);
+      firstColumn.add(Integer.valueOf(firstNumber));
+      secondColumn.add(Integer.valueOf(secondNumber));
     }
+
+    Collections.sort(firstColumn);
+    Collections.sort(secondColumn);
+
+    int similarity = 0;
+
+    for (Integer firstNumber: firstColumn){
+
+      int occurences = 0;
+      for (Integer secondNumber: secondColumn) {
+        if (firstNumber.intValue() == secondNumber.intValue()){
+          occurences++;
+        }
+      }
+
+      similarity += firstNumber * occurences;
+    }
+
+    System.out.println(similarity);
   }
 }

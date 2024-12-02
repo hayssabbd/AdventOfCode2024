6d3d1f57560e5f59b3a8bddb3ec8a339e62043a5
Craig Deacon <craig.deacon@bbd.ca>
2024-12-02 12:21:51 -0500

    Day 2 Puzzle 2

--- Main.java
+++ Main.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import org.w3c.dom.ls.LSOutput;
 
 public class Main {
 
@@ -29,43 +28,76 @@ public class Main {
       String[] numberStrings = line.split(" ");
       int[] numberArray = Arrays.stream(numberStrings).mapToInt(Integer::parseInt).toArray();
 
-      boolean increasingNumbers = numberArray[0] < numberArray[1];
-      boolean decreasingNumbers = numberArray[0] > numberArray[1];
-      boolean safe = true;
 
-      for (int ctr = 0; ctr < numberArray.length; ctr++) {
-        if (ctr < numberArray.length - 1) {
-          int currentNumber = numberArray[ctr];
-          int nextNumber = numberArray[ctr + 1];
-
-          if (currentNumber == nextNumber){
-            safe = false;
-            break;
-          }
-          if (Math.abs(currentNumber - nextNumber) > 3) {
-            safe = false;
-            break;
-          }
-          if (increasingNumbers) {
-            if (currentNumber > nextNumber) {
-              safe = false;
-              break;
-            }
-            //unnecessary as decreasing numbers will always be true by this point, but more clear
-          } else if (decreasingNumbers) {
-            if (currentNumber < nextNumber) {
-              safe = false;
-              break;
-            }
-          }
-        }
-      }
-
-      if (safe){
+      if (isReportSafe(numberArray)) {
         safeCount++;
+      } else {
+        if(tryAllPermutations(numberArray)){
+          safeCount++;
+        }
       }
     }
     System.out.println(safeCount);
+  }
+
+  private static int[] removeIndex(int[] numberArray, int index) {
+    int[] newArray = new int[numberArray.length -1];
+
+    for(int ctr = 0; ctr < numberArray.length; ctr++){
+      if (ctr < index){
+        newArray[ctr] = numberArray[ctr];
+      } else if (ctr > index){
+        newArray[ctr -1] = numberArray[ctr];
+      }
+    }
+    return newArray;
+  }
+
+  public static boolean isReportSafe(int[] numberArray) {
+    boolean safe = true;
+    boolean increasingNumbers = numberArray[0] < numberArray[1];
+    boolean decreasingNumbers = numberArray[0] > numberArray[1];
+    for (int ctr = 0; ctr < numberArray.length; ctr++) {
+      if (ctr < numberArray.length - 1) {
+        int currentNumber = numberArray[ctr];
+        int nextNumber = numberArray[ctr + 1];
+
+        if (currentNumber == nextNumber) {
+          safe = false;
+          break;
+        }
+        if (Math.abs(currentNumber - nextNumber) > 3) {
+          safe = false;
+          break;
+        }
+        if (increasingNumbers) {
+          if (currentNumber > nextNumber) {
+            safe = false;
+            break;
+          }
+          //unnecessary as decreasing numbers will always be true by this point, but more clear
+        } else if (decreasingNumbers) {
+          if (currentNumber < nextNumber) {
+            safe = false;
+            break;
+          }
+        }
+      }
+    }
+
+    return safe;
+  }
+
+  private static boolean tryAllPermutations(int[] numberArray) {
+    boolean safe = false;
+    for (int ctr = 0; ctr < numberArray.length; ctr++){
+     int[] permutation = removeIndex(numberArray, ctr);
+     safe = isReportSafe(permutation);
+     if(safe){
+       break;
+     }
+    }
+    return safe;
   }
 }
 
